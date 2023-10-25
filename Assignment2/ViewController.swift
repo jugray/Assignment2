eb83ca226a1aac08a715b783ad4bc50c366fe2ef
Justin Gray <jugray@gmail.com>
2023-10-24 23:33:38 -0500

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Justin Gray on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+   
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
